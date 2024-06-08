97993f90c6a6e9116a260c71b88e6bc16a597d9f
Mikołaj Lemański <mikolajlem@gmail.com>
2024-06-08 21:41:15 +0200

    Added school icon

--- src/pages/MapPage/components/components/PageMarkers.tsx
+++ src/pages/MapPage/components/components/PageMarkers.tsx
@@ -1,7 +1,12 @@
 import { AHttpClient } from '@/http/AxiosAbstract';
-import { Marker } from 'react-leaflet/Marker';
+import {
+    Circle,
+    Marker,
+} from 'react-leaflet';
+import { Icon } from 'leaflet';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import school from '/school.svg';
 
 type Props = {
     page: number;
@@ -22,14 +27,28 @@ const PageMarkers = ({ page }: Props) => {
     });
 
     return (data?.['hydra:member'] || []).map((location, index) => (
-        <Marker
-            key={index}
-            position={[
-                location.geolokalizacja.latitude,
-                location.geolokalizacja.longitude,
-            ]}
-        >
-        </Marker>
+        <>
+            <Marker
+                icon={new Icon({ iconAnchor: [
+                    32,
+                    32,
+                ], iconUrl: school })}
+                key={index}
+                position={[
+                    location.geolokalizacja.latitude,
+                    location.geolokalizacja.longitude,
+                ]}
+            >
+            </Marker>
+
+            <Circle
+                center={{ lat: location.geolokalizacja.latitude, lng: location.geolokalizacja.longitude }}
+                fillColor="blue"
+                fillRule="inherit"
+                radius={1000}
+                weight={1}
+            />
+        </>
     ));
 };
 
